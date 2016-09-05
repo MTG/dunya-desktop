6c13c0e61ffcab7539d53df60af5f75818069025
hsercanatli <hsercanatli@gmail.com>
2016-09-05 17:18:53 +0200

    Table widget

---
+++ widgets/table.py
@@ -0,0 +1,25 @@
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+
+class TableWidget(QtGui.QTableWidget):
+    def __init__(self, *__args):
+        QtGui.QTableWidget.__init__(self, *__args)
+
+        # setting the table for no edit and row selection
+        self.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        self.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+
+        # hiding the vertical headers
+        self.verticalHeader().hide()
+
+        # arranging the artist column for being multi-line
+        self.setWordWrap(True)
+        self.setTextElideMode(QtCore.Qt.ElideMiddle)
+
+        self.set_css()
+
+    def set_css(self):
+        with open("../ui_files/css/tableview.css") as f:
+            css = f.read()
+        self.setStyleSheet(css)
