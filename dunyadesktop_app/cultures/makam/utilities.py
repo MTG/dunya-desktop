0490186c5f42657a4112ebfa36e5419ab0ff3e2a
hsercanatli <hsercanatli@gmail.com>
2016-09-12 15:07:31 +0200

    sorting the attributes is refactored

--- dunyadesktop_app/cultures/makam/utilities.py
+++ dunyadesktop_app/cultures/makam/utilities.py
@@ -4,21 +4,21 @@ from dunyadesktop_app.utilities.utilities import *
 
 def get_attributes():
     makams = compmusic.dunya.makam.get_makams()
-    sort_dictionary(makams, 'name')
+    makams = sort_dictionary(makams, 'name')
 
     forms = compmusic.dunya.makam.get_forms()
-    sort_dictionary(forms, 'name')
+    forms = sort_dictionary(forms, 'name')
 
     usuls = compmusic.dunya.makam.get_usuls()
-    sort_dictionary(usuls, 'name')
+    usuls = sort_dictionary(usuls, 'name')
 
     composers = compmusic.dunya.makam.get_composers()
-    sort_dictionary(composers, 'name')
+    composers = sort_dictionary(composers, 'name')
 
     performers = compmusic.dunya.makam.get_artists()
-    sort_dictionary(performers, 'name')
+    performers = sort_dictionary(performers, 'name')
 
     instruments = compmusic.dunya.makam.get_instruments()
-    sort_dictionary(instruments, 'name')
+    instruments = sort_dictionary(instruments, 'name')
 
     return makams, forms, usuls, composers, performers, instruments
