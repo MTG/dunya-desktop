d921d46c6901c002c68f1038b498e4dc20e9e801
hsercanatli <hsercanatli@gmail.com>
2016-09-12 15:08:53 +0200

    attributes set method is added

--- dunyadesktop_app/mainui_makam.py
+++ dunyadesktop_app/mainui_makam.py
@@ -1,8 +1,8 @@
 import sys
 from cultures import token
-from cultures import makam
+from cultures.makam import utilities
 
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
 from mainui_design_makam import MainWindowMakamDesign
 
 token.set_token()
@@ -12,6 +12,17 @@ class MainWindowMakam(MainWindowMakamDesign):
     def __init__(self):
         MainWindowMakamDesign.__init__(self)
 
+        (self.makams, self.usuls, self.forms, self.composers,
+         self.performers, self.instruments) = utilities.get_attributes()
+        self._set_combobox_attributes()
+
+    def _set_combobox_attributes(self):
+        self.frame_attributes.comboBox_melodic.add_items(self.makams)
+        self.frame_attributes.comboBox_form.add_items(self.forms)
+        self.frame_attributes.comboBox_rhythm.add_items(self.usuls)
+        self.frame_attributes.comboBox_composer.add_items(self.composers)
+        self.frame_attributes.comboBox_performer.add_items(self.performers)
+        self.frame_attributes.comboBox_instrument.add_items(self.instruments)
 
 app = QtGui.QApplication(sys.argv)
 mainwindow_makam = MainWindowMakam()
