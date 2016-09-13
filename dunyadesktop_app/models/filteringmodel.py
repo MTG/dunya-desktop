ec3765dc85b5a1da701e8282968eab70e0dfa4a5
hsercanatli <hsercanatli@gmail.com>
2016-09-13 15:20:23 +0200

    filtering model is added

---
+++ dunyadesktop_app/models/filteringmodel.py
@@ -0,0 +1,19 @@
+from PyQt4 import QtGui, QtCore
+
+
+class FilteringModel(QtGui.QStandardItemModel):
+
+    def __init__(self):
+        QtGui.QStandardItemModel.__init__(self)
+
+    def add_items(self, attribute):
+        self.setRowCount(len(attribute))
+
+        for row, item in enumerate(attribute):
+            name = QtGui.QStandardItem(item['name'])
+
+            check_item = QtGui.QStandardItem()
+            check_item.setCheckable(True)
+
+            self.setItem(row, 0, check_item)
+            self.setItem(row, 1, name)
