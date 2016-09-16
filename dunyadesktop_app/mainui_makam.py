3ab340c0879d6ea3c8b7f84bdcec5840f6d9b579
hsercanatli <hsercanatli@gmail.com>
2016-09-16 19:15:15 +0200

    query button signal is created

--- dunyadesktop_app/mainui_makam.py
+++ dunyadesktop_app/mainui_makam.py
@@ -1,6 +1,7 @@
 import sys
 from cultures import apiconfig
 from cultures.makam import utilities
+from compmusic.dunya import makam
 
 from PyQt4 import QtGui
 from mainui_design_makam import MainWindowMakamDesign
@@ -16,6 +17,8 @@ class MainWindowMakam(MainWindowMakamDesign):
          self.performers, self.instruments) = utilities.get_attributes()
         self._set_combobox_attributes()
 
+        self.frame_attributes.toolButton_query.clicked.connect(self.query)
+
     def _set_combobox_attributes(self):
         self.frame_attributes.comboBox_melodic.add_items(self.makams)
         self.frame_attributes.comboBox_form.add_items(self.forms)
@@ -23,6 +26,16 @@ class MainWindowMakam(MainWindowMakamDesign):
         self.frame_attributes.comboBox_composer.add_items(self.composers)
         self.frame_attributes.comboBox_performer.add_items(self.performers)
         self.frame_attributes.comboBox_instrument.add_items(self.instruments)
+
+    def query(self):
+        mid = self.frame_attributes.comboBox_melodic.get_attribute_id()
+        fid = self.frame_attributes.comboBox_form.get_attribute_id()
+        uid = self.frame_attributes.comboBox_rhythm.get_attribute_id()
+        cmbid = self.frame_attributes.comboBox_performer.get_attribute_id()
+        ambid = self.frame_attributes.comboBox_composer.get_attribute_id()
+
+        print makam.get_works_by_query(mid, uid, fid, cmbid, ambid)
+
 
 app = QtGui.QApplication(sys.argv)
 mainwindow_makam = MainWindowMakam()
