abac640b2601282604f427654399ba379715fd35
hsercanatli <hsercanatli@gmail.com>
2016-09-19 19:20:51 +0200

    Artist and composer ids are refactored

--- dunyadesktop_app/mainui_makam.py
+++ dunyadesktop_app/mainui_makam.py
@@ -12,6 +12,7 @@ from mainui_design_makam import MainWindowMakamDesign
 apiconfig.set_token()
 apiconfig.set_hostname()
 
+
 class MainWindowMakam(MainWindowMakamDesign):
     def __init__(self):
         MainWindowMakamDesign.__init__(self)
@@ -35,11 +36,11 @@ class MainWindowMakam(MainWindowMakamDesign):
         mid = self.frame_attributes.comboBox_melodic.get_attribute_id()
         fid = self.frame_attributes.comboBox_form.get_attribute_id()
         uid = self.frame_attributes.comboBox_rhythm.get_attribute_id()
-        cmbid = self.frame_attributes.comboBox_performer.get_attribute_id()
-        ambid = self.frame_attributes.comboBox_composer.get_attribute_id()
+        cmbid = self.frame_attributes.comboBox_composer.get_attribute_id()
+        ambid = self.frame_attributes.comboBox_performer.get_attribute_id()
 
-        print makam.get_works_by_query(mid, uid, fid, cmbid, ambid)
-
+        print makam.get_works_by_query(mid=mid, uid=uid, fid=fid, cmbid=cmbid,
+                                       ambid=ambid)
 
 app = QtGui.QApplication(sys.argv)
 mainwindow_makam = MainWindowMakam()
