b385549a5f4687f255f6311acfd1ed5b302d03bd
hsercanatli <hsercanatli@gmail.com>
2016-09-22 13:54:00 +0200

    query module is moved to makam

---
+++ dunyadesktop_app/cultures/makam/query.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+from PyQt4 import QtCore
+from compmusic.dunya import makam
+
+
+class QueryThread(QtCore.QThread):
+    query_completed = QtCore.pyqtSignal()
+
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+        self.stopped = False
+
+        self.mid = None
+        self.uid = None
+        self.fid = None
+        self.cmbid = None
+        self.ambid = None
+
+        self.works = None
+
+    def run(self):
+        self.works = makam.get_works_by_query(mid=self.mid, uid=self.uid,
+                                              fid=self.fid, cmbid=self.cmbid,
+                                              ambid=self.ambid)
+        for xx, work in enumerate(self.works):
+            print(xx, work)
+
+        self.query_completed.emit()
