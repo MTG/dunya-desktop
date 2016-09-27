982b2b652a8da62bd906aab56adf5058b24cf42f
hsercanatli <hsercanatli@gmail.com>
2016-09-27 12:50:15 +0200

    modification if add_recording function and new signal -a new signal for adding the recordings in main window -table items are modified

--- dunyadesktop_app/models/recordingmodel.py
+++ dunyadesktop_app/models/recordingmodel.py
@@ -4,14 +4,34 @@ from PyQt4 import QtGui, QtCore
 
 
 class RecordingModel(QtGui.QStandardItemModel):
+    rec_fetched = QtCore.pyqtSignal(str)
+
     def __init__(self):
         QtGui.QStandardItemModel.__init__(self)
-        self.setColumnCount(2)
+        self.setHorizontalHeaderLabels(['', 'Title', 'Artists'])
 
     def add_recording(self, work):
         for rec in work['recordings']:
-            artists = rec['artists']
-            mbid = QtGui.QStandardItem(rec['mbid'])
+            check_item = QtGui.QStandardItem()
+            check_item.setCheckable(True)
+
             title = QtGui.QStandardItem(rec['title'])
-            self.insertRow(self.rowCount(), title)
-            self.setItem(self.rowCount()-1, 1, mbid)
+
+            artists = ''
+            rec['artists'] = [dict(tupleized) for tupleized in
+                              set(tuple(element.items())
+                                  for element in rec['artists'])]
+
+            for artist in rec['artists']:
+                artists += artist['name'] + ", "
+
+            artists = artists[:-2]
+            artist_item = QtGui.QStandardItem(artists)
+
+            mbid = rec['mbid']
+            self.rec_fetched.emit(mbid)
+
+            self.insertRow(self.rowCount())
+            self.setItem(self.rowCount() - 1, 0, check_item)
+            self.setItem(self.rowCount() - 1, 1, title)
+            self.setItem(self.rowCount() - 1, 2, artist_item)
