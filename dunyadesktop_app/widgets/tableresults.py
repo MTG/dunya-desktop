d2bc0158aad36c2fa48652f45bc61478a6f3ba37
hsercanatli <hsercanatli@gmail.com>
2016-10-03 13:19:10 +0200

    Contextmenu is created and "open on dunya" action is added

--- dunyadesktop_app/widgets/tableresults.py
+++ dunyadesktop_app/widgets/tableresults.py
@@ -1,22 +1,31 @@
+import os
+
 from PyQt4 import QtGui
 
 from .table import TableView
 
+dunya_icon = os.path.join(os.path.dirname(__file__), '..', 'ui_files',
+                                'icons', 'dunya.svg')
 
 class TableViewResults(TableView):
     def __init__(self, parent=None):
         TableView.__init__(self)
-
         self.setSortingEnabled(True)
 
         self.horizontal_header = self.horizontalHeader()
+        self._set_horizontal_header()
+
+        self.menu = QtGui.QMenu(self)
+        open_dunya = QtGui.QAction("Open on Player", self)
+        open_dunya.setIcon(QtGui.QIcon(dunya_icon))
+        self.menu.addAction(open_dunya)
+
+    def _set_horizontal_header(self):
         self.horizontal_header.setStretchLastSection(True)
         self.horizontal_header.hide()
 
-        self.menu = QtGui.QMenu(self)
-
     def contextMenuEvent(self, event):
         if self.selectionModel().selection().indexes():
-            for i in self.selectionModel().selection().indexes():
-                row, column = i.row(), i.column()
-        print(row)
+            for index in self.selectionModel().selection().indexes():
+                row, column = index.row(), index.column()
+        self.menu.popup(QtGui.QCursor.pos())
