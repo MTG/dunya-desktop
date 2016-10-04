07f03f6f332afbc123c0059acc03758d6e87b0d7
hsercanatli <hsercanatli@gmail.com>
2016-10-04 12:58:11 +0200

    Waveform widget layout and size policy are set

--- dunyadesktop_app/widgets/waveformwidget.py
+++ dunyadesktop_app/widgets/waveformwidget.py
@@ -1,4 +1,4 @@
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from pyqtgraph import GraphicsLayoutWidget
 import pyqtgraph as pg
 
@@ -6,29 +6,19 @@ import pyqtgraph as pg
 class WaveformWidget(GraphicsLayoutWidget):
     def __init__(self):
         GraphicsLayoutWidget.__init__(self, parent=None)
-        self._set_waveform()
 
         self.layout = pg.GraphicsLayout()
-        self._set_layout()
+        self._set_size_policy()
 
-        self.waveform = self.addPlot(title='Waveform')
-        self.waveform.setDownsampling(auto=True)
-        self.waveform.hideAxis(axis='bottom')
-        self.waveform.hideAxis(axis='left')
+    def _set_size_policy(self):
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred,
+                                       QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(100)
+        sizePolicy.setHeightForWidth(
+            self.sizePolicy().hasHeightForWidth())
 
-        self.waveform.setMaximumHeight(100)
-        self.waveform.setMouseEnabled(x=False, y=False)
-
-
-    def _set_waveform(self):
-        size_policy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred,
-                                        QtGui.QSizePolicy.Fixed)
-        size_policy.setHorizontalStretch(0)
-        size_policy.setVerticalStretch(100)
-        size_policy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
-        self.setSizePolicy(size_policy)
-
-    def _set_layout(self):
-        self.layout = pg.GraphicsLayout(border=(100, 100, 100))
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.setSpacing(0)
+        self.setMinimumSize(QtCore.QSize(0, 100))
+        self.setMaximumSize(QtCore.QSize(16777215, 100))
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.setAcceptDrops(False)
