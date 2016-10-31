401d2418cd38e96edf208726dcd9559e585eb59d
hsercanatli <hsercanatli@gmail.com>
2016-10-31 12:53:06 +0100

    Refactored and documented

--- dunyadesktop_app/widgets/dockwidget.py
+++ dunyadesktop_app/widgets/dockwidget.py
@@ -29,11 +29,13 @@ CSS_TABLEVIEW_DOWNLOADED = os.path.join(os.path.dirname(__file__), '..',
 
 
 class DockWidget(QtGui.QDockWidget):
+    """Dockwidget for the main window"""
     def __init__(self, min_width, min_height, max_width, max_height):
         QtGui.QDockWidget.__init__(self)
         self._set_dockwidget(min_width, min_height, max_width, max_height)
 
     def _set_dockwidget(self, min_width, min_height, max_width, max_height):
+        """Sets the size policies of the dock widget"""
         sizepolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred,
                                        QtGui.QSizePolicy.Minimum)
         sizepolicy.setHorizontalStretch(0)
@@ -49,57 +51,59 @@ class DockWidget(QtGui.QDockWidget):
 
 
 class DockWidgetContentsLeft(QtGui.QWidget):
+    """Contains the contents of the dock widget on the left side of the main
+    window"""
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self._set_widget()
-        #self._set_css(self, CSS_DOCKWIDGET)
 
-        self.vertical_layout = QtGui.QVBoxLayout(self)
-        self.vertical_layout.setContentsMargins(4, 0, 0, 0)
-        self.vertical_layout.setSpacing(0)  # check it
+        layout = QtGui.QVBoxLayout(self)
+        layout.setContentsMargins(4, 0, 0, 0)
+        layout.setSpacing(0)  # check it
 
         self.frame_collection = QtGui.QFrame(self)
         self._set_frame()
 
-        self.vertical_layout3 = QtGui.QVBoxLayout(self.frame_collection)
-        self.vertical_layout3.setContentsMargins(2, 5, 3, 2)
-        self.vertical_layout3.setSpacing(7)  # check it
+        layout_3 = QtGui.QVBoxLayout(self.frame_collection)
+        layout_3.setContentsMargins(2, 5, 3, 2)
+        layout_3.setSpacing(7)  # check it
 
         self.label_collections = QtGui.QLabel(self.frame_collection)
         self.label_collections.setIndent(15)  # check it
         self.label_collections.setTextInteractionFlags(
             QtCore.Qt.NoTextInteraction)
         self._set_css(self.label_collections, CSS_LABEL_COLLECTION)
-        self.vertical_layout3.addWidget(self.label_collections)
+        layout_3.addWidget(self.label_collections)
 
         # listview (seperate it)
         self.listView_collections = QtGui.QListView(self.frame_collection)
         self._set_css(self.listView_collections, CSS_LISTVIEW)
         self.listView_collections.setViewMode(QtGui.QListView.ListMode)
-        self.vertical_layout3.addWidget(self.listView_collections)
-        self.vertical_layout.addWidget(self.frame_collection)
+        layout_3.addWidget(self.listView_collections)
+        layout.addWidget(self.frame_collection)
 
         # toolbutton
         self.toolButton_collection = QtGui.QToolButton(self)
         self._set_toolbutton()
-        self.vertical_layout.addWidget(self.toolButton_collection)
+        layout.addWidget(self.toolButton_collection)
 
         self.frame_downloaded = QtGui.QFrame(self)
         self._set_frame_downloaded()
 
-        self.vertical_layout4 = QtGui.QVBoxLayout(self.frame_downloaded)
-        self.vertical_layout4.setContentsMargins(3, 5, 3, 2)
+        layout_4 = QtGui.QVBoxLayout(self.frame_downloaded)
+        layout_4.setContentsMargins(3, 5, 3, 2)
         self.label_downloaded = QtGui.QLabel(self.frame_downloaded)
         self._set_label_downloaded()
-        self.vertical_layout4.addWidget(self.label_downloaded)
+        layout_4.addWidget(self.label_downloaded)
 
         self.tableView_downloaded = QtGui.QTableView(self.frame_downloaded)
         self._set_css(self.tableView_downloaded, CSS_TABLEVIEW_DOWNLOADED)
-        self.vertical_layout4.addWidget(self.tableView_downloaded)
-        self.vertical_layout.addWidget(self.frame_downloaded)
+        layout_4.addWidget(self.tableView_downloaded)
+        layout.addWidget(self.frame_downloaded)
         self.retranslateUi()
 
     def _set_widget(self):
+        """Sets the size policies."""
         sizepolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
                                        QtGui.QSizePolicy.Preferred)
         sizepolicy.setHorizontalStretch(0)
@@ -115,6 +119,7 @@ class DockWidgetContentsLeft(QtGui.QWidget):
         obj.setStyleSheet(css)
 
     def _set_frame(self):
+        """Sets the size policies of the frame."""
         sizepolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
                                        QtGui.QSizePolicy.Preferred)
         sizepolicy.setHorizontalStretch(0)
@@ -131,6 +136,7 @@ class DockWidgetContentsLeft(QtGui.QWidget):
         self.frame_collection.setFrameShadow(QtGui.QFrame.Raised)
 
     def _set_toolbutton(self):
+        """Sets the size policies of the new collection button."""
         sizepolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred,
                                        QtGui.QSizePolicy.Fixed)
         sizepolicy.setHorizontalStretch(0)
@@ -148,6 +154,7 @@ class DockWidgetContentsLeft(QtGui.QWidget):
         self.toolButton_collection.setArrowType(QtCore.Qt.NoArrow)
 
     def _set_frame_downloaded(self):
+        """Sets the size policies of the downloaded features frame."""
         self.frame_downloaded = QtGui.QFrame(self)
         sizepolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
                                        QtGui.QSizePolicy.Preferred)
@@ -163,6 +170,7 @@ class DockWidgetContentsLeft(QtGui.QWidget):
         self.frame_downloaded.setFrameShadow(QtGui.QFrame.Raised)
 
     def _set_label_downloaded(self):
+        """Sets the label 'Downloaded'."""
         font = QtGui.QFont()
         font.setFamily("Garuda")
         self.label_downloaded.setFont(font)
@@ -180,40 +188,42 @@ class DockWidgetContentsLeft(QtGui.QWidget):
 
 
 class DockWidgetContentsTop(QtGui.QWidget):
+    """Contains the contents of the dock widget on the top side of the main
+        window"""
     def __init__(self):
         QtGui.QWidget.__init__(self)
 
-        self.horizontal_layout = QtGui.QHBoxLayout(self)
-        self.horizontal_layout.setContentsMargins(2, 0, 4, 0)
-        self.horizontal_layout.setSpacing(5)  # check it
+        layout = QtGui.QHBoxLayout(self)
+        layout.setContentsMargins(2, 0, 4, 0)
+        layout.setSpacing(5)  # check it
 
         spacer = QtGui.QSpacerItem(20, 0, QtGui.QSizePolicy.Expanding,
                                    QtGui.QSizePolicy.Fixed)
-
-        self.horizontal_layout.addItem(spacer)
+        layout.addItem(spacer)
 
         self.label_corpus = QtGui.QLabel(self)
         self._set_label_corpus()
-        self.horizontal_layout.addWidget(self.label_corpus)
+        layout.addWidget(self.label_corpus)
 
         spacer2 = QtGui.QSpacerItem(20, 0, QtGui.QSizePolicy.Expanding,
                                     QtGui.QSizePolicy.Fixed)
-        self.horizontal_layout.addItem(spacer2)
+        layout.addItem(spacer2)
 
         self.label_username = QtGui.QLabel(self)
         self.label_username.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
-        self.horizontal_layout.addWidget(self.label_username)
+        layout.addWidget(self.label_username)
 
         self.line = QtGui.QFrame(self)
         self._set_line()
-        self.horizontal_layout.addWidget(self.line)
+        layout.addWidget(self.line)
 
         self.label_status = QtGui.QLabel(self)
         self._set_labelstatus()
-        self.horizontal_layout.addWidget(self.label_status)
-        self.retranslateUi()
+        layout.addWidget(self.label_status)
+        self.retranslate_ui()
 
     def _set_label_corpus(self):
+        """Sets the label"""
         font = QtGui.QFont()
         font.setFamily("Garuda")
         self.label_corpus.setFont(font)
@@ -228,6 +238,7 @@ class DockWidgetContentsTop(QtGui.QWidget):
         self.label_corpus.setMaximumSize(QtCore.QSize(700, 30))
 
     def _set_line(self):
+        """Vertical line between the status and username"""
         sizepolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed,
                                        QtGui.QSizePolicy.Fixed)
         sizepolicy.setHorizontalStretch(0)
@@ -241,6 +252,7 @@ class DockWidgetContentsTop(QtGui.QWidget):
         self.line.setFrameShadow(QtGui.QFrame.Sunken)
 
     def _set_labelstatus(self):
+        """Sets the size policy of label status"""
         sizepolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed,
                                        QtGui.QSizePolicy.Fixed)
         sizepolicy.setHorizontalStretch(0)
@@ -249,7 +261,7 @@ class DockWidgetContentsTop(QtGui.QWidget):
             self.label_status.sizePolicy().hasHeightForWidth())
         self.label_status.setSizePolicy(sizepolicy)
 
-    def retranslateUi(self):
+    def retranslate_ui(self):
         self.label_username.setText("<html><head/><body><p align=\"right\"><span style=\" font-weight:600; color:#7c7c7c;\">user.name</span></p></body></html>")
         self.label_status.setText('<html><head/><body><p><span style=" font-size:10pt; color:#73ff7c;">online</span></p></body></html>')
         self.label_corpus.setText('<html><head/><body><p align="center"><span style=" font-size:15pt; color:#C1C1C1;">CORPUS</span></p></body></html>')
