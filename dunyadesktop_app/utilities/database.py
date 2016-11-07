bee93811949fbb3a917419e0f5e5373f2cf7482c
hsercanatli <hsercanatli@gmail.com>
2016-11-07 11:59:41 +0100

    Changes in add_collection and connect functions -add collections returns true of false according to creation if given collection name -added default (main_collection) collection to the database

--- dunyadesktop_app/utilities/database.py
+++ dunyadesktop_app/utilities/database.py
@@ -4,20 +4,24 @@ import sqlite3
 DB_PATH = os.path.join(os.path.dirname(__file__), '..', 'db.sqlite')
 
 
-def connect():
-    # connecting to the database
-    # if database is not exist, it creates
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    return conn, c
-
-
 def add_collection(conn, c, list_name):
     try:
         # creating the collections table
         c.execute("CREATE TABLE {tn} (DOCID TEXT)".format(tn=list_name))
         conn.commit()
         conn.close()
-        print(list_name, 'added')
+        return True
     except:
-        pass
+        return False
+
+
+def connect():
+    # connecting to the database
+    # if database is not exist, it creates
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    # adding the main collection
+    add_collection(conn, c, 'MainCollection')
+
+    return conn, c
