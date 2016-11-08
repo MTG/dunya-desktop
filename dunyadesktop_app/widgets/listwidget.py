6723de820737028ef40716ec9323c73c4c798657
hsercanatli <hsercanatli@gmail.com>
2016-11-08 15:06:31 +0100

    Added an abstract list widget class -Other list widgets inherits the abstract one

--- dunyadesktop_app/widgets/listwidget.py
+++ dunyadesktop_app/widgets/listwidget.py
@@ -1,14 +1,16 @@
 import os
+import json
 
 from PyQt4 import QtGui, QtCore
 
 CSS_LISTVIEW = os.path.join(os.path.dirname(__file__), '..', 'ui_files',
                             'css', 'listwidget.css')
 
+DOCS_PATH = os.path.join(os.path.dirname(__file__), '..', 'cultures',
+                         'documents')
 
-class CollectionsWidget(QtGui.QListWidget):
-    index_changed = QtCore.pyqtSignal(str)
 
+class DockListWidget(QtGui.QListWidget):
     def __init__(self):
         QtGui.QListView.__init__(self)
 
@@ -20,15 +22,22 @@ class CollectionsWidget(QtGui.QListWidget):
         self.setAcceptDrops(True)
         self.setDragEnabled(True)
 
-        # signals
-        self.index = self.currentIndex().row()
-        self.itemClicked.connect(self.item_clicked)
 
     @staticmethod
     def _set_css(obj, css_path):
         with open(css_path) as f:
             css = f.read()
         obj.setStyleSheet(css)
+
+
+class CollectionsWidget(DockListWidget):
+    index_changed = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        DockListWidget.__init__(self)
+        # signals
+        self.index = self.currentIndex().row()
+        self.itemClicked.connect(self.item_clicked)
 
     def add_collections(self, colls):
         for coll in colls:
@@ -39,3 +48,18 @@ class CollectionsWidget(QtGui.QListWidget):
         if not self.currentIndex().row() is self.index:
             self.index = self.currentIndex().row()
             self.index_changed.emit(self.item(self.index).text())
+
+
+class CollectionList(DockListWidget):
+    def __init__(self):
+        DockListWidget.__init__(self)
+
+    def add_items(self, coll):
+        # first cleans all the items on the list
+        self.clear()
+        for item in coll:
+            path = os.path.join(DOCS_PATH, item,
+                                'audioanalysis--metadata.json')
+            metadata = json.load(open(path))
+            item = QtGui.QListWidgetItem(metadata['title'])
+            self.addItem(item)
