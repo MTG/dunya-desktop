7476b1592e15ec2653e862e54396112cba7542e9
hsercanatli <hsercanatli@gmail.com>
2016-11-22 16:35:02 +0100

    Created a specific action class for collections

--- dunyadesktop_app/widgets/menu.py
+++ dunyadesktop_app/widgets/menu.py
@@ -10,9 +10,19 @@ DUNYA_ICON = os.path.join(os.path.dirname(__file__), '..', 'ui_files',
                           'icons', 'dunya.svg')
 
 
+class CollectionAction(QtGui.QAction):
+    def __init__(self, name, parent=None):
+        QtGui.QAction.__init__(self, name, parent)
+        self.triggered.connect(self.send_coll)
+
+    def send_coll(self):
+        self.parent()._come_signal(self.text())
+
+
 class RCMenu(QtGui.QMenu):
     def __init__(self, parent=None):
-        QtGui.QMenu.__init__(self, parent)
+        self.parent = parent
+        QtGui.QMenu.__init__(self, self.parent)
         #self._set_css()
         self._add_actions()
 
@@ -26,16 +36,16 @@ class RCMenu(QtGui.QMenu):
         self.open_dunya.setIcon(QtGui.QIcon(DUNYA_ICON))
         self.addAction(self.open_dunya)
 
-        collections_menu = self.addMenu('Collections')
-
-        self.add_maincoll = QtGui.QAction("Add to main collection", self)
-        collections_menu.addAction(self.add_maincoll)
-        collections_menu.addSeparator()
+        self.addSeparator()
+        collections_menu = self.addMenu('Add to collection')
 
         conn, c = database.connect()
         collections = database.get_collections(c)
 
         for coll in collections:
-            act = QtGui.QAction(str(coll[0]), self)
+            act = CollectionAction(str(coll[0]), self)
             collections_menu.addAction(act)
         conn.close()
+
+    def _come_signal(self, coll):
+        print(coll)
