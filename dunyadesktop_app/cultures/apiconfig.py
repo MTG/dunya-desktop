e5c6dd2df24ec97531d9f8e176c7868a79e042d3
Hasan Sercan Atlı <hsercanatli@gmail.com>
2017-01-02 10:03:19 +0200

    Code is refactored according to UTF8

--- dunyadesktop_app/cultures/apiconfig.py
+++ dunyadesktop_app/cultures/apiconfig.py
@@ -4,17 +4,17 @@ import ConfigParser
 import compmusic.dunya.conn
 
 
-def set_token():
+def _get_option(option):
     config = ConfigParser.ConfigParser()
     config.read(os.path.join(os.path.dirname(__file__), 'config.cfg'))
-    DUNYA_TOKEN = config.get('dunya', 'token')
+    return config.get('dunya', option)
 
+
+def set_token():
+    DUNYA_TOKEN = _get_option('token')
     compmusic.dunya.conn.set_token(DUNYA_TOKEN)
 
 
 def set_hostname():
-    config = ConfigParser.ConfigParser()
-    config.read(os.path.join(os.path.dirname(__file__), 'config.cfg'))
-    DUNYA_HOSTNAME = config.get('dunya', 'hostname')
-
+    DUNYA_HOSTNAME = _get_option('hostname')
     compmusic.dunya.conn.set_hostname(DUNYA_HOSTNAME)
