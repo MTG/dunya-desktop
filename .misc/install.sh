5eea1aa45a5c95589148d28fa9b0688a1e943303
Hasan Sercan Atlı <hsercanatli@gmail.com>
2017-01-20 19:27:05 +0100

    Updated install.sh for PyQt5 (travis-ci) test2

--- .misc/install.sh
+++ .misc/install.sh
@@ -8,17 +8,19 @@ SIP_VERSION=4.19
 
 # Install sip
 wget --retry-connrefused https://sourceforge.net/projects/pyqt/files/sip/sip-$SIP_VERSION/sip-$SIP_VERSION.tar.gz
-tar -xzvf sip-$SIP_VERSION.tar.gz
+tar -xzf sip-$SIP_VERSION.tar.gz
 cd sip-$SIP_VERSION
 python configure.py
 make
 sudo make install
 cd ..
+which sip
+which qmake
 
 # Install PyQt5
 wget --retry-connrefused https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-$PYQT_VERSION/PyQt5_gpl-$PYQT_VERSION.tar.gz
-tar -xzvf PyQt5_gpl-$PYQT_VERSION.tar.gz
+tar -xzf PyQt5_gpl-$PYQT_VERSION.tar.gz
 cd PyQt5_gpl-$PYQT_VERSION
-python configure.py --confirm-license
+python configure.py --confirm-license --qmake=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
 make
 sudo make install
