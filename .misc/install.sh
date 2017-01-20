01f42268068577a3dba7157697adb461df799331
Hasan Sercan Atlı <hsercanatli@gmail.com>
2017-01-20 19:16:29 +0100

    Updated install.sh for PyQt5 (travis-ci)

--- .misc/install.sh
+++ .misc/install.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
-sudo apt-get install libxext-dev python-qt4 qt4-dev-tools build-essential
+
+sudo apt-get update
+sudo apt-get install tar build-essential qt5-default qt5-qmake libqt5webkit5-dev
+
+PYQT_VERSION=5.7.1
+SIP_VERSION=4.19
+
+# Install sip
+wget --retry-connrefused https://sourceforge.net/projects/pyqt/files/sip/sip-$SIP_VERSION/sip-$SIP_VERSION.tar.gz
+tar -xzvf sip-$SIP_VERSION.tar.gz
+cd sip-$SIP_VERSION
+python configure.py
+make
+sudo make install
+cd ..
+
+# Install PyQt5
+wget --retry-connrefused https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-$PYQT_VERSION/PyQt5_gpl-$PYQT_VERSION.tar.gz
+tar -xzvf PyQt5_gpl-$PYQT_VERSION.tar.gz
+cd PyQt5_gpl-$PYQT_VERSION
+python configure.py --confirm-license
+make
+sudo make install
