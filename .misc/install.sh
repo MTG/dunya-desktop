0ba2f9045fccf9073406ff33fa46ad0a1a686f64
Hasan Sercan Atlı <hsercanatli@gmail.com>
2017-01-20 19:46:59 +0100

    Updated install.sh for PyQt5 (travis-ci) test5

--- .misc/install.sh
+++ .misc/install.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 1397BC53640DB551
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository --yes ppa:beineri/opt-qt57-trusty
-
-sudo apt-get install -qq g++-6 qt57-meta-full
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
-sudo apt-get install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
-sudo apt-get install qt5-default qttools5-dev-tools
+sudo apt-get install -qq libqt5webkit5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
+export QMAKE=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
 
 PYQT_VERSION=5.7.1
 SIP_VERSION=4.19
