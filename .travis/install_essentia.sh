d67dc810963036ff4fd419053807d3d1c669bedd
Hasan Sercan Atlı <hsercanatli@gmail.com>
2017-02-07 10:07:35 +0100

    Added essentia installation steps

---
+++ .travis/install_essentia.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo apt-get install build-essential libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev
+sudo apt-get install python-numpy-dev python-numpy python-yaml
+
+wget https://github.com/MTG/essentia/archive/v2.1_beta2.tar.gz
+
+tar -xzf v2.1_beta2.tar.gz
+cd essentia-2.1_beta2
+./waf configure --mode=release --build-static --with-python
+./waf
+sudo ./waf install
+
+python -c 'import essentia'
