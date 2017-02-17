8f560759612ca17122acdace24a0a5598b942b86
Hasan Sercan Atlı <hsercanatli@gmail.com>
2017-02-17 13:46:16 +0100

    Refactored the parser functions

--- dunyadesktop_app/cultures/makam/svgparser.py
+++ dunyadesktop_app/cultures/makam/svgparser.py
@@ -1,27 +1,27 @@
 import xml.etree.ElementTree as ET
 
-
 def initialize_svg(path):
     tree = ET.parse(path)
     return tree, tree.getroot()
 
+SVG_NS = '{http://www.w3.org/2000/svg}'
 
 def change_color(path, tree, root, note_id, color):
-    for child in root.iter('{http://www.w3.org/2000/svg}a'):
+    note_id = 'note-' + str(note_id)
+    for a in root.findall(SVG_NS + 'a'):
         try:
-            if child.attrib['id'] == 'note-' + str(note_id):
-                note = child.find('{http://www.w3.org/2000/svg}path')
-                note.set('fill', color)
+            if a.attrib['id'] == note_id:
+                note_color_parameter = a.find(SVG_NS + 'path')
+                note_color_parameter.set('fill', color)
         except KeyError:
             pass
-
     tree.write(path)
 
 def get_note_indexes(path, root):
     notes = {}
-    for child in root.iter('{http://www.w3.org/2000/svg}a'):
+    for a in root.findall(SVG_NS + 'a'):
         try:
-            index = child.attrib['id'].split('note-')[-1]
+            index = a.attrib['id'].split('note-')[-1]
             notes[index] = path
         except KeyError:
             pass
