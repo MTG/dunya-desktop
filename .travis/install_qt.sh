d43305441e11741268e95ff848195bfaf40742ce
Hasan Sercan Atlı <hsercanatli@gmail.com>
2017-02-27 10:18:56 +0100

    refactored qmake path

--- .travis/install_qt.sh
+++ .travis/install_qt.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq qtdeclarative5-dev libqt5svg5-dev
-export QMAKE=QMAKE=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
+export QMAKE=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
 
 PYQT_VERSION=5.7.1
 SIP_VERSION=4.19
@@ -21,7 +21,7 @@ cd ..
 wget --retry-connrefused https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-$PYQT_VERSION/PyQt5_gpl-$PYQT_VERSION.tar.gz
 tar -xzf PyQt5_gpl-$PYQT_VERSION.tar.gz
 cd PyQt5_gpl-$PYQT_VERSION
-python configure.py --confirm-license --qmake=/usr/bin/qmake-qt5 -e QtCore -e QtGui -e QtWidgets -e QtTest -e QtOpenGL -e QtSvg
+python configure.py --confirm-license --qmake=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake -e QtCore -e QtGui -e QtWidgets -e QtTest -e QtOpenGL -e QtSvg
 make
 sudo make install
 
