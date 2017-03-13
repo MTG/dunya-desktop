0c60e61be60b716a771c0299e0d9f8ef6c1eccb4
hsercanatli <hsercanatli@gmail.com>
2017-03-13 11:53:06 +0100

    Added seperate if for 3.5

--- .travis/install_pyqt_python35.sh
+++ .travis/install_pyqt_python35.sh
@@ -3,10 +3,23 @@
 # Install PyQt5 for Python 3
 pip3 install PyQt5
 
+echo 'QtWidgets'
 python3 -c 'from PyQt5 import QtWidgets'
+
+echo 'QtCore'
 python3 -c 'from PyQt5 import QtCore'
+
+echo 'QtTest'
 python3 -c 'from PyQt5 import QtTest'
+
+echo 'QtGui'
 python3 -c 'from PyQt5 import QtGui'
+
+echo 'Qt'
 python3 -c 'from PyQt5 import Qt'
+
+echo 'QtSvg'
 python3 -c 'from PyQt5 import QtSvg'
+
+echo 'QtMultimedia'
 python3 -c 'from PyQt5 import QtMultimedia'
