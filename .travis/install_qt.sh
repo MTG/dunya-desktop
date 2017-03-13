730b06eb514b6dee422a0d3035cd42bd50703207
hsercanatli <hsercanatli@gmail.com>
2017-03-13 15:17:00 +0100

    Added repo for ffmpeg

--- .travis/install_qt.sh
+++ .travis/install_qt.sh
@@ -4,5 +4,9 @@
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq qtdeclarative5-dev libqt5svg5-dev qtmultimedia5-dev
+
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update -qq
 sudo apt-get install -qq ffmpeg
+
 export QMAKE=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
