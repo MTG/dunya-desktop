e140f36613304b351c5081bce5003abbdd3a8838
hsercanatli <hsercanatli@gmail.com>
2017-03-13 15:20:59 +0100

    Added yes option

--- .travis/install_qt.sh
+++ .travis/install_qt.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq qtdeclarative5-dev libqt5svg5-dev qtmultimedia5-dev
 
-sudo add-apt-repository ppa:mc3man/trusty-media
+sudo add-apt-repository --yes ppa:mc3man/trusty-media
 sudo apt-get update -qq
 sudo apt-get install -qq ffmpeg
 
