eb8f6ca141c5c3e345943867ae8415f0dd251112
hsercanatli <hsercanatli@gmail.com>
2017-03-13 14:23:20 +0100

    Added ffmpeg to qt installation script

--- .travis/install_qt.sh
+++ .travis/install_qt.sh
@@ -4,4 +4,5 @@
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq qtdeclarative5-dev libqt5svg5-dev qtmultimedia5-dev
+sudo apt-get install -qq ffmpeg
 export QMAKE=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
