44b47fadc5cdcf4ab760223dcdf010b69f4576d1
Niccolò Pretto <niccolopretto@users.noreply.github.com>
2017-12-04 12:30:12 +0100

    Corrected the plotting function cumpute_overall_histogram

--- dunyadesktop_app/utilities/corpusbasestatistics.py
+++ dunyadesktop_app/utilities/corpusbasestatistics.py
@@ -41,21 +41,109 @@ class Converter(object):
 
 
 def compute_overall_histogram(histograms):
-    overall_hist = {}
+    values = {}
+    tonic = {}
+    bins_in_cent = {}
+    new_x_axis_in_cent = {}
+    interpolated_values = {}
+    curves = {}
+
+    min_bin_value = 0
+    max_bin_value = 0
+
     for mbid in histograms:
-        valley = histograms[mbid][0][0]
-        bins = histograms[mbid][0][1]
-        tonic = histograms[mbid][1]
+        print("Curves: ", mbid)
+        # translate histogram in cent
+        values[mbid] = histograms[mbid][0][0]
+        tonic[mbid] = histograms[mbid][1]
+        bins_in_cent[mbid] = Converter.hz_to_cent(histograms[mbid][0][1], tonic[mbid])
 
-        for ii in range(len(bins)):
-            cent_bin = Converter.hz_to_cent(bins[ii], tonic)[0]
+        # update the overall minimum and maximum bounds in cents
+        if int(bins_in_cent[mbid][0]) < min_bin_value:
+            min_bin_value = int(bins_in_cent[mbid][0])
 
-            try:
-                overall_hist[cent_bin] = overall_hist[cent_bin] + valley[ii]
-            except KeyError:
-                overall_hist[cent_bin] = valley[ii]
+        if int(bins_in_cent[mbid][-1]) > max_bin_value:
+            max_bin_value = int(bins_in_cent[mbid][-1])
 
-    plot_bins = sorted(overall_hist.keys())
+        print("Min: ", int(bins_in_cent[mbid][0]))
+        print("Max: ", int(bins_in_cent[mbid][-1]))
+
+        # create the new x axis interval in cent for every function
+        new_x_axis_in_cent[mbid] = range(int(bins_in_cent[mbid][0]), int(bins_in_cent[mbid][-1]), 1)
+        print("New x: ", new_x_axis_in_cent[mbid])
+
+        # use interpolation to find all the values on the new x axis
+        interpolated_values[mbid] = np.interp(new_x_axis_in_cent[mbid], bins_in_cent[mbid], values[mbid])
+
+        # create a dictionary for the curves
+        curves[mbid] = dict(zip(new_x_axis_in_cent[mbid], interpolated_values[mbid]))
+
+    overall_x = range(min_bin_value, max_bin_value)
+    overall_y = np.zeros(max_bin_value - min_bin_value)
+
+    overall_hist = dict(zip(overall_x, overall_y))
+
+    for mbid in curves:
+        for key in curves[mbid]:
+            overall_hist[key] += curves[mbid][key]
+
+    print("Results")
+    print("Min: ", min_bin_value)
+    print("Max: ", max_bin_value)
+
+    plot_bins = list(overall_hist.keys())
     plot_vals = [overall_hist[key] for key in plot_bins]
 
     pg.plot(plot_bins, plot_vals)
+
+
+# def compute_overall_histogram(histograms):
+#     overall_hist = {}
+#     values = {}
+#     bins = {}
+#     tonic = {}
+#
+#     min_bin_value = 0
+#     max_bin_value = 0
+#
+#     for mbid in histograms:
+#         values[mbid] = histograms[mbid][0][0]
+#         bins[mbid] = histograms[mbid][0][1]
+#         tonic[mbid] = histograms[mbid][1]
+#
+#     # find the minimum and maximum bounds in cents
+#     for mbid in bins:
+#
+#         temp_bounds = [bins[mbid][0], bins[mbid][-1]]
+#         cent_temp_bounds = Converter.hz_to_cent(temp_bounds, tonic)
+#
+#         if cent_temp_bounds[0] < min_bin_value:
+#             min_bin_value = cent_temp_bounds[0]
+#
+#         if cent_temp_bounds[1] > max_bin_value:
+#             max_bin_value = cent_temp_bounds[1]
+#
+#     # create the x-axis array
+#     x = arange(min_bin_value, max_bin_value, 1)
+#
+#
+#
+#
+#     plot_bins = sorted(overall_hist.keys())
+#     plot_vals = [overall_hist[key] for key in plot_bins]
+#
+#     pg.plot(plot_bins, plot_vals)
+#
+#     # for mbid in histograms:
+#     #     valley = histograms[mbid][0][0]
+#     #     bins = histograms[mbid][0][1]
+#     #     tonic = histograms[mbid][1]
+#     #
+#     #     for ii in range(len(bins)):
+#     #         cent_bin = Converter.hz_to_cent(bins[ii], tonic)[0]
+#     #         #cent_bin.astype(int)
+#     #
+#     #         try:
+#     #             overall_hist[cent_bin] = overall_hist[cent_bin] + valley[ii]
+#     #         except KeyError:
+    #             overall_hist[cent_bin] = valley[ii]
