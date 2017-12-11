3ee0aaa62c14d9b18bb98f132a743c5a9f7a7cbf
Niccolò Pretto <niccolopretto@users.noreply.github.com>
2017-12-11 18:23:12 +0100

    Debug codes deleted. Comment added to compute_overall_histogram function

--- dunyadesktop_app/utilities/corpusbasestatistics.py
+++ dunyadesktop_app/utilities/corpusbasestatistics.py
@@ -41,6 +41,16 @@ class Converter(object):
 
 
 def compute_overall_histogram(histograms):
+    """--------------------------------------------------------------------
+    Plot an overall histogram obtained by the sum of the histograms provided
+    as inputs. Every histogram is converted in cents. A new x-axis interval
+    is created using minimum and maximum value in cents for all the
+    histograms. All the values of the histograms are inserted in the new
+    x-axis interpolating them from the original reference system.
+    -----------------------------------------------------------------------
+    histograms: array of histogram. Every histogram is defined as
+    [[vals, bins], tonic]
+    --------------------------------------------------------------------"""
     values = {}
     tonic = {}
     bins_in_cent = {}
@@ -52,7 +62,7 @@ def compute_overall_histogram(histograms):
     max_bin_value = 0
 
     for mbid in histograms:
-        print("Curves: ", mbid)
+
         # translate histogram in cent
         values[mbid] = histograms[mbid][0][0]
         tonic[mbid] = histograms[mbid][1]
@@ -65,12 +75,8 @@ def compute_overall_histogram(histograms):
         if int(bins_in_cent[mbid][-1]) > max_bin_value:
             max_bin_value = int(bins_in_cent[mbid][-1])
 
-        print("Min: ", int(bins_in_cent[mbid][0]))
-        print("Max: ", int(bins_in_cent[mbid][-1]))
-
         # create the new x axis interval in cent for every function
         new_x_axis_in_cent[mbid] = range(int(bins_in_cent[mbid][0]), int(bins_in_cent[mbid][-1]), 1)
-        print("New x: ", new_x_axis_in_cent[mbid])
 
         # use interpolation to find all the values on the new x axis
         interpolated_values[mbid] = np.interp(new_x_axis_in_cent[mbid], bins_in_cent[mbid], values[mbid])
@@ -86,10 +92,6 @@ def compute_overall_histogram(histograms):
     for mbid in curves:
         for key in curves[mbid]:
             overall_hist[key] += curves[mbid][key]
-
-    print("Results")
-    print("Min: ", min_bin_value)
-    print("Max: ", max_bin_value)
 
     plot_bins = list(overall_hist.keys())
     plot_vals = [overall_hist[key] for key in plot_bins]
@@ -125,9 +127,6 @@ def compute_overall_histogram(histograms):
 #
 #     # create the x-axis array
 #     x = arange(min_bin_value, max_bin_value, 1)
-#
-#
-#
 #
 #     plot_bins = sorted(overall_hist.keys())
 #     plot_vals = [overall_hist[key] for key in plot_bins]
